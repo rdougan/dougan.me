76b59fac9b1f26b827b50bad61ede0a3ef0301e8
Robert Dougan <Robert@Mac-Pro.config>
2009-02-28 16:27:11 +0000

    adding admin routes

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,4 +2,9 @@ class IndexController < ApplicationController
   def index
     
   end
+  
+  def admin
+    dir = "public/admin#{RAILS_ENV == 'production' ? '/public' : ''}"
+    send_file "#{dir}/index.html", :disposition => 'inline', :filename => 'admin', :type => 'text/html'
+  end
 end
