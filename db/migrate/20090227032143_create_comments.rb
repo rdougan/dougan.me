efdc6605b843fe7d28944a895f976e1116633489
Robert Dougan <Robert@Mac-Pro.config>
2009-03-01 03:51:10 +0000

    updating the styling of new comment and various other little things

--- db/migrate/20090227032143_create_comments.rb
+++ db/migrate/20090227032143_create_comments.rb
@@ -1,7 +1,6 @@
 class CreateComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
-      t.integer :post_id
       t.string :name
       t.string :email
       t.text :message
