e3f9c140506d52a45392b4f3320ed757608dfaf1
Robert Dougan <Robert@Mac-Pro.config>
2009-03-01 04:04:44 +0000

    oops

--- db/migrate/20090227032143_create_comments.rb
+++ db/migrate/20090227032143_create_comments.rb
@@ -1,6 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
+      t.integer :post_id
       t.string :name
       t.string :email
       t.text :message
